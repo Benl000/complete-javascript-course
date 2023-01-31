e098625a66c797e295f6fb813e92e0c105eabc30
Ben <Benl000100@gmail.com>
2023-02-01 00:14:33 +0200

    finishing proj 06 modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,9 +4,15 @@ let openModalBtn = document.querySelectorAll('.show-modal');
 let overlay = document.querySelector('.overlay');
 let closeModalBtn = document.querySelector('.close-modal');
 
+//open modal
 openModalBtn.forEach(button => button.addEventListener('click', openModal));
+
+//close modal
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', (ev) => {
+    if ((ev.key === 'Escape') && (!document.querySelector('.modal').classList.contains('hidden'))) closeModal();
+});
 
 function openModal() {
     document.querySelector('.modal').classList.remove('hidden');
